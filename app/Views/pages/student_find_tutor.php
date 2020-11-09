11fe4bef5070bb66acd7b1bd64ecaa5ad7b163ab
railee091 <74151748+railee091@users.noreply.github.com>
2020-11-10 00:51:55 +0800

    minor change
    student fine tutore

--- app/Views/pages/student_find_tutor.php
+++ app/Views/pages/student_find_tutor.php
@@ -221,43 +221,15 @@
 						<div class="container">
 							<div class="row">
 								<div class="card" style="width: 15rem;">
-									<img class="card-img-top" src="../images/filler_images/stock_female.png" alt="Card image cap">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-										<a href="#" class="btn btn-primary">Go somewhere</a>
-									</div>
-								</div>
-								<div class="card" style="width: 15rem;">
 									<img class="card-img-top" src="../images/filler_images/stock_male.png" alt="Card image cap">
 									<div class="card-body">
 										<h5 class="card-title">Card title</h5>
 										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-										<a href="#" class="btn btn-primary">Go somewhere</a>
-									</div>
-								</div>
-								<div class="card" style="width: 15rem;">
-									<img class="card-img-top" src="../images/filler_images/stock_female.png" alt="Card image cap">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-										<a href="#" class="btn btn-primary">Go somewhere</a>
-									</div>
-								</div>
-								<div class="card" style="width: 15rem;">
-									<img class="card-img-top" src="../images/filler_images/stock_male.png" alt="Card image cap">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-										<a href="#" class="btn btn-primary">Go somewhere</a>
-									</div>
-								</div>
-								<div class="card" style="width: 15rem;">
-									<img class="card-img-top" src="../images/filler_images/stock_male.png" alt="Card image cap">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-										<a href="#" class="btn btn-primary">Go somewhere</a>
+										<div class="row">	
+											<div class="col-sm">
+												<a title="see profile" href="#" class="btn btn-primary" data-toggle="modal" data-target="#teacherIdModal" ><i class="fas fa-user"></i></a>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
