e728e377983e2e8c6e7067c5c266fbe0e179b695
railee091 <74151748+railee091@users.noreply.github.com>
2020-11-10 21:57:29 +0800

    link in login page
    just to have access to dashboard designs

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -19,6 +19,8 @@
 				<a class="navbar-brand" href="#">
 				LOGO
 				</a>
+				<a href="app/views/pages/student_dashboard.php">DEVstudent</a>
+				<a href="app/views/pages/teacher_dashboard.php">DEVteacher</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>	
