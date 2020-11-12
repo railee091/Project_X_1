55fc6d6aba48c0ee71bf25b0a35ed7918434ba0f
railee091 <74151748+railee091@users.noreply.github.com>
2020-11-12 23:29:09 +0800

    student profile
    updated elements
    -info
    -about me /intro
    -image gallery
    -linked videos

--- app/Views/pages/student_profile.php
+++ app/Views/pages/student_profile.php
@@ -31,12 +31,24 @@
         <div id="main">
 			<?php require "navigation/top_nav.php"; ?>
 			<div class="col-md">
+			<div class="card">
 				<a href="teacher_dashboard.php"><button type="button" class="btn btn-primary">BACK</button></a>
+			</div><br/>
+			<div class="card">
+				<div class="card-body">
+					<div class="row">
+						<div class="col-md-12">
+							<h4>Your Student Profile</h4>
+						</div>
+					</div>	
+				</div>
+			</div>
+			<br/>
 				<div class="card">
 					<div class="card-body">
 						<div class="row">
 							<div class="col-md-12">
-								<h4>Your Profile</h4>
+								<h4>Public Info</h4>
 								<hr>
 							</div>
 						</div>
@@ -93,7 +105,7 @@
 								  </div> 
 								  <div class="form-group row">
 									<div class="offset-4 col-8">
-									  <button name="submit" type="submit" class="btn btn-primary">Update My Profile</button>
+									  <button name="submit" type="submit" class="btn btn-primary">save</button>
 									</div>
 								  </div>
 								</form>
@@ -102,8 +114,276 @@
 						
 					</div>
 				</div>
+				<br/>
+				<div class="card">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-12">
+								<h4>Say something about you</h4>
+								<hr>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<form>
+								  <div class="form-group row">
+									<label for="publicinfo" class="col-4 col-form-label">Intro</label> 
+									<div class="col-8">
+									  <textarea id="publicinfo" name="publicinfo" cols="40" rows="4" class="form-control"></textarea>
+									</div>
+								  </div>
+								  <div class="form-group row">
+									<label for="publicinfo" class="col-4 col-form-label">About Me</label> 
+									<div class="col-8">
+									  <textarea id="publicinfo" name="publicinfo" cols="40" rows="4" class="form-control"></textarea>
+									</div>
+								  </div>
+								  <div class="form-group row">
+									<div class="offset-4 col-8">
+									  <button name="submit" type="submit" class="btn btn-primary">save</button>
+									</div>
+								  </div>
+								</form>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+				<br/>
+				<div class="card">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-12">
+								<h4>Your Album</h4>
+								<hr>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+							<form>
+							  <div class="form-group">
+								<label for="exampleFormControlFile1">Example file input</label>
+								<input type="file" class="form-control-file" id="exampleFormControlFile1">
+							  </div>
+							</form>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="container">
+								  <div class="row">
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="0"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="1"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="2"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="3"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="4"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="5"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="6"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6" data-toggle="modal" data-target="#modal">
+									  <a href="#lightbox" data-slide-to="7"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="img-thumbnail my-3"></a>
+									</div>    
+								  </div>
+								</div>
+								<!-- Modal -->
+								<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="Lightbox Gallery by Bootstrap 4" aria-hidden="true">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-body">
+												<div id="lightbox" class="carousel slide" data-ride="carousel" data-interval="5000" data-keyboard="true">
+													<ol class="carousel-indicators">
+														<li data-target="#lightbox" data-slide-to="0"></li>
+														<li data-target="#lightbox" data-slide-to="1"></li>
+														<li data-target="#lightbox" data-slide-to="2"></li>
+														<li data-target="#lightbox" data-slide-to="3"></li>
+														<li data-target="#lightbox" data-slide-to="4"></li>
+														<li data-target="#lightbox" data-slide-to="5"></li>
+														<li data-target="#lightbox" data-slide-to="6"></li>
+														<li data-target="#lightbox" data-slide-to="7"></li>
+													</ol>
+													<div class="carousel-inner">
+														<div class="carousel-item active"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+														<div class="carousel-item"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+														<div class="carousel-item"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+														<div class="carousel-item"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+														<div class="carousel-item"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+														<div class="carousel-item"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+														<div class="carousel-item"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+														<div class="carousel-item"><img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="w-100"
+															 alt=""></div>
+													</div>
+													<a class="carousel-control-prev" href="#lightbox" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a>
+													<a class="carousel-control-next" href="#lightbox" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br/>
+				<div class="card">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-12">
+								<h4>Linked Videos</h4>
+								<hr>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<!-- Grid row -->
+								<div class="row">
+
+								  <!-- Grid column -->
+								  <div class="col-lg-4 col-md-12 mb-4">
+
+									<!--Modal: Name-->
+									<div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+									  <div class="modal-dialog modal-lg" role="document">
+
+										<!--Content-->
+										<div class="modal-content">
+
+										  <!--Body-->
+										  <div class="modal-body mb-0 p-0">
+
+											<div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+											  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUEZCxBcM78"
+												allowfullscreen></iframe>
+											</div>
+
+										  </div>
+
+										  <!--Footer-->
+										  <div class="modal-footer justify-content-center">
+											 
+											<button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+
+										  </div>
+
+										</div>
+										<!--/.Content-->
+
+									  </div>
+									</div>
+									<!--Modal: Name-->
+
+									<a><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-4.jpg" alt="video"
+										data-toggle="modal" data-target="#modal2"></a>
+
+								  </div>
+								  <!-- Grid column -->
+
+								  <!-- Grid column -->
+								  <div class="col-lg-4 col-md-6 mb-4">
+
+									<!--Modal: Name-->
+									<div class="modal fade" id="modal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+									  <div class="modal-dialog modal-lg" role="document">
+
+										<!--Content-->
+										<div class="modal-content">
+
+										  <!--Body-->
+										  <div class="modal-body mb-0 p-0">
+
+											<div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+											  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/PjGkVCAo8Fw"
+												allowfullscreen></iframe>
+											</div>
+
+										  </div>
+
+										  <!--Footer-->
+										  <div class="modal-footer justify-content-center">
+											 
+											<button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+
+										  </div>
+
+										</div>
+										<!--/.Content-->
+
+									  </div>
+									</div>
+									<!--Modal: Name-->
+
+									<a><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-5.jpg" alt="video"
+										data-toggle="modal" data-target="#modal5"></a>
+
+								  </div>
+								  <!-- Grid column -->
+
+								  <!-- Grid column -->
+								  <div class="col-lg-4 col-md-6 mb-4">
+
+									<!--Modal: Name-->
+									<div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+									  <div class="modal-dialog modal-lg" role="document">
+
+										<!--Content-->
+										<div class="modal-content">
+
+										  <!--Body-->
+										  <div class="modal-body mb-0 p-0">
+
+											<div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+											  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gsNY7TV4r1M"
+												allowfullscreen></iframe>
+											</div>
+
+										  </div>
+
+										  <!--Footer-->
+										  <div class="modal-footer d-block d-md-flex justify-content-center">
+											 
+											<button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+
+										  </div>
+
+										</div>
+										<!--/.Content-->
+
+									  </div>
+									</div>
+									<!--Modal: Name-->
+
+									<a><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-6.jpg" alt="video"
+										data-toggle="modal" data-target="#modal3"></a>
+
+								  </div>
+								  <!-- Grid column -->
+
+								</div>
+								<!-- Grid row -->
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			
         </div>
     </div>
 
