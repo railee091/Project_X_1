6c1e9ef386f005d9720741ffa9ded35e6e39e60b
railee091 <74151748+railee091@users.noreply.github.com>
2020-12-06 16:34:55 +0800

    my classes interface

--- app/Views/student/student_my_classes.php
+++ app/Views/student/student_my_classes.php
@@ -7,165 +7,60 @@
 							<header class="card-header"><h6 class="title">Category </h6></header>
 							<div class="filter-content">
 								<div class="list-group list-group-flush">
-								  <a href="#" class="list-group-item">Groups<span class="float-right badge badge-light round">142</span> </a>
-								  <a href="#" class="list-group-item">Following<span class="float-right badge badge-light round">3</span>  </a>
-								  <a href="#" class="list-group-item">Public<span class="float-right badge badge-light round">32</span>  </a>
-								  <a href="#" class="list-group-item">Announcement<span class="float-right badge badge-light round">12</span>  </a>
+								  <a href="#" class="list-group-item">Classes<span class="float-right badge badge-light round">5</span> </a>
+								  <a href="#" class="list-group-item">Tutors<span class="float-right badge badge-light round">3</span>  </a>
+								  <a href="#" class="list-group-item">History<span class="float-right badge badge-light round">32</span>  </a>
+								  <a href="#" class="list-group-item">Files<span class="float-right badge badge-light round">12</span>  </a>
 								</div>  <!-- list-group .// -->
 							</div>
 						</article> <!-- card-group-item.// -->
 					</div> <!-- card.// -->
 				</div>
 				
-				<div class="col-md-6 gedf-main column-bg">
-					<!--- \\\\\\\Post-->
-					<div class="card gedf-card">
-						<div class="card-header">
-							<ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
-								<li class="nav-item">
-									<a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts" aria-selected="true">Make
-										a publication</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" id="images-tab" data-toggle="tab" role="tab" aria-controls="images" aria-selected="false" href="#images">Images</a>
-								</li>
-							</ul>
-						</div>
-						<div class="card-body">
-							<div class="tab-content" id="myTabContent">
-								<div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
-									<div class="form-group">
-										<label class="sr-only" for="message">post</label>
-										<textarea class="form-control" id="message" rows="3" placeholder="What are you thinking?"></textarea>
-									</div>
-
-								</div>
-								<div class="tab-pane fade" id="images" role="tabpanel" aria-labelledby="images-tab">
-									<div class="form-group">
-										<div class="custom-file">
-											<input type="file" class="custom-file-input" id="customFile">
-											<label class="custom-file-label" for="customFile">Upload image</label>
+				<div class="center-column col-md-6 gedf-main column-bg">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<ul class="event-list">
+									<?php  
+										for ($x = 0; $x <= 4; $x++) {//this just loops the list item
+											$imageCount = $x;
+									?>
+									<li>
+										<time datetime="2014-07-20 2000">
+											<span class="day">20</span>
+											<span class="month">Jan</span>
+											<span class="year">2021</span>
+											<span class="time">8:00 PM</span>
+										</time>
+										<img alt="My 24th Birthday!" src="https://i.pinimg.com/originals/bd/b0/40/bdb0409f08d0ffd8bc732f4110cc6ad9.jpg" />
+										<div class="info">
+											<h2 class="title">Booked class</h2>
+											<p class="desc">You have a class with Teacher Jane Doe on July 20, 2021 (wednesday) at 8:00pm</p>
+											<ul>
+												<li style="width:33%;">1</li>
+												<li style="width:34%;">2</span></li>
+												<li style="width:33%;"><span class="fa fa-envelope"></span></li>
+											</ul>
 										</div>
-									</div>
-									<div class="py-4"></div>
-								</div>
-							</div>
-							<div class="btn-toolbar justify-content-between">
-								<div class="btn-group">
-									<button type="submit" class="btn btn-primary">share</button>
-								</div>
-								<div class="btn-group">
-									<button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-										aria-expanded="false">
-										<i class="fa fa-globe"></i> Public
-									</button>
-									<div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-										<a class="dropdown-item" href="#"><i class="fa fa-globe"></i> Public</a>
-										<a class="dropdown-item" href="#"><i class="fa fa-users"></i> Friends</a>
-										<a class="dropdown-item" href="#"><i class="fa fa-user"></i> Just me</a>
-									</div>
-								</div>
+										<div class="social">
+											<ul>
+												<li class="facebook" style="width:33%;"><a href="#facebook"><span class="fa fa-address-card" title="Tutor Profile"></span></a></li>
+												<li class="twitter" style="width:34%;"><a href="#twitter"><span class="fa fa-history" title="Class History"></span></a></li>
+												<li class="google-plus" style="width:33%;"><a href="#google-plus"><span class="fa fa-window-close" title="Cancel Class"></span></a></li>
+											</ul>
+										</div>
+									</li>
+									<?php	
+										} //this just loops the list item
+									?> 
+								</ul>
 							</div>
 						</div>
 					</div>
-					<!-- Post /////-->
-					<br/>
-					<!--- \\\\\\\Post-->
-					<div class="card gedf-card">
-						<div class="card-header">
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="d-flex justify-content-between align-items-center">
-									<div class="mr-2 ">
-										<img class="feedImageTutor rounded-circle" width="45"  height="45" src="https://pyxis.nymag.com/v1/imgs/357/bbc/cea6584b56451bc29339e57fcf820b3281-30-chris-hemsworth.rsquare.w1200.jpg" alt="">
-									</div>
-									<div class="ml-2">
-										<div class="h5 m-0">@Chris Hemsworth</div>
-										<div class="h7 text-muted">ESL Tutor | God of Thunder</div>
-									</div>
-								</div>
-								<div>
-									<div class="dropdown">
-										<button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-											<i class="fa fa-ellipsis-h"></i>
-										</button>
-										<div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
-											<div class="h6 dropdown-header">Configuration</div>
-											<a class="dropdown-item" href="#">unfollow</a>
-											<a class="dropdown-item" href="#">Hide</a>
-											<a class="dropdown-item" href="#">Report</a>
-										</div>
-									</div>
-								</div>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
-							<a class="card-link" href="#">
-								<h5 class="card-title">The Guardians need a Leader.</h5>
-							</a>
-
-							<p class="card-text">
-								The Rabbit Is Correct, And Clearly The Smartest One Among You.
-							</p>
-						</div>
-						<div class="card-footer">
-							<a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
-							<a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
-							<a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
-						</div>
-					</div>
-					<!-- Post /////-->
-					<br/>
-					<!--- \\\\\\\Post-->
-					<div class="card gedf-card">
-						<div class="card-header">
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="d-flex justify-content-between align-items-center">
-									<div class="mr-2">
-										<img class="feedImageStudent rounded-circle" width="45" height="45" src="https://hairstylesweekly.com/images/2013/11/Natalie-Portman.jpg" alt="">
-									</div>
-									<div class="ml-2">
-										<div class="h5 m-0">@Natalie Portman</div>
-										<div class="h7 text-muted">Student| Queen of Naboo</div>
-									</div>
-								</div>
-								<div>
-									<div class="dropdown">
-										<button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-											<i class="fa fa-ellipsis-h"></i>
-										</button>
-										<div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
-											<div class="h6 dropdown-header">Configuration</div>
-											<a class="dropdown-item" href="#">unfollow</a>
-											<a class="dropdown-item" href="#">Hide</a>
-											<a class="dropdown-item" href="#">Report</a>
-										</div>
-									</div>
-								</div>
-							</div>
-
-						</div>
-						<div class="card-body">
-							<div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
-							<a class="card-link" href="#">
-								<h5 class="card-title">The Senator Palpatine Dilemma</h5>
-							</a>
-
-							<p class="card-text">
-								Senator, this is your arena. I feel I must return my mine. I have decided to go back to Naboo.
-							</p>
-						</div>
-						<div class="card-footer">
-							<a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
-							<a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
-							<a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
-						</div>
-					</div>
-					<!-- Post /////-->
 				</div>
 				<div class="col-md-3">
-					<div class="alert alert-warning alert-dismissible fade show" role="alert">
+					<!--<div class="alert alert-warning alert-dismissible fade show" role="alert">
 					  <strong>System Upgrade!</strong> System has been updated to latest version
 					  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
@@ -182,16 +77,198 @@
 					  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					  </button>
-					</div>
+					</div>-->
 				</div>
-				<style>
-.feedImageTutor{
-	border-style: solid;
-	border-color: yellowgreen;
-}
-.feedImageStudent{
-	border-style: solid;
-	border-color: Blue;
-}
+<style>
+
+    .center-column {
+		padding: 60px 0px;
+		background-color: rgb(220, 220, 220);
+	}
+    
+    .event-list {
+		list-style: none;
+		font-family: 'Lato', sans-serif;
+		margin: 0px;
+		padding: 0px;
+	}
+	.event-list > li {
+		background-color: rgb(255, 255, 255);
+		box-shadow: 0px 0px 5px rgb(51, 51, 51);
+		box-shadow: 0px 0px 5px rgba(51, 51, 51, 0.7);
+		padding: 0px;
+		margin: 0px 0px 20px;
+	}
+	.event-list > li > time {
+		display: inline-block;
+		width: 100%;
+		color: rgb(255, 255, 255);
+		background-color: rgb(197, 44, 102);
+		padding: 5px;
+		text-align: center;
+		text-transform: uppercase;
+	}
+	.event-list > li:nth-child(even) > time {
+		background-color: rgb(165, 82, 167);
+	}
+	.event-list > li > time > span {
+		display: none;
+	}
+	.event-list > li > time > .day {
+		display: block;
+		font-size: 56pt;
+		font-weight: 100;
+		line-height: 1;
+	}
+	.event-list > li time > .month {
+		display: block;
+		font-size: 24pt;
+		font-weight: 900;
+		line-height: 1;
+	}
+	.event-list > li > img {
+		width: 100%;
+	}
+	.event-list > li > .info {
+		padding-top: 5px;
+		text-align: center;
+	}
+	.event-list > li > .info > .title {
+		font-size: 17pt;
+		font-weight: 700;
+		margin: 0px;
+	}
+	.event-list > li > .info > .desc {
+		font-size: 13pt;
+		font-weight: 300;
+		margin: 0px;
+	}
+	.event-list > li > .info > ul,
+	.event-list > li > .social > ul {
+		display: table;
+		list-style: none;
+		margin: 10px 0px 0px;
+		padding: 0px;
+		width: 100%;
+		text-align: center;
+	}
+	.event-list > li > .social > ul {
+		margin: 0px;
+	}
+	.event-list > li > .info > ul > li,
+	.event-list > li > .social > ul > li {
+		display: table-cell;
+		cursor: pointer;
+		color: rgb(30, 30, 30);
+		font-size: 11pt;
+		font-weight: 300;
+        padding: 3px 0px;
+	}
+    .event-list > li > .info > ul > li > a {
+		display: block;
+		width: 100%;
+		color: rgb(30, 30, 30);
+		text-decoration: none;
+	} 
+    .event-list > li > .social > ul > li {    
+        padding: 0px;
+    }
+    .event-list > li > .social > ul > li > a {
+        padding: 3px 0px;
+	} 
+	.event-list > li > .info > ul > li:hover,
+	.event-list > li > .social > ul > li:hover {
+		color: rgb(30, 30, 30);
+		background-color: rgb(200, 200, 200);
+	}
+	.facebook a,
+	.twitter a,
+	.google-plus a {
+		display: block;
+		width: 100%;
+		color: rgb(75, 110, 168) !important;
+	}
+	.twitter a {
+		color: rgb(79, 213, 248) !important;
+	}
+	.google-plus a {
+		color: rgb(221, 75, 57) !important;
+	}
+	.facebook:hover a {
+		color: rgb(255, 255, 255) !important;
+		background-color: rgb(75, 110, 168) !important;
+	}
+	.twitter:hover a {
+		color: rgb(255, 255, 255) !important;
+		background-color: rgb(79, 213, 248) !important;
+	}
+	.google-plus:hover a {
+		color: rgb(255, 255, 255) !important;
+		background-color: rgb(221, 75, 57) !important;
+	}
+
+	@media (min-width: 768px) {
+		.event-list > li {
+			position: relative;
+			display: block;
+			width: 100%;
+			height: 120px;
+			padding: 0px;
+		}
+		.event-list > li > time,
+		.event-list > li > img  {
+			display: inline-block;
+		}
+		.event-list > li > time,
+		.event-list > li > img {
+			width: 120px;
+			float: left;
+		}
+		.event-list > li > .info {
+			background-color: rgb(245, 245, 245);
+			overflow: hidden;
+		}
+		.event-list > li > time,
+		.event-list > li > img {
+			width: 120px;
+			height: 120px;
+			padding: 0px;
+			margin: 0px;
+		}
+		.event-list > li > .info {
+			position: relative;
+			height: 120px;
+			text-align: left;
+			padding-right: 40px;
+		}	
+		.event-list > li > .info > .title, 
+		.event-list > li > .info > .desc {
+			padding: 0px 10px;
+		}
+		.event-list > li > .info > ul {
+			position: absolute;
+			left: 0px;
+			bottom: 0px;
+		}
+		.event-list > li > .social {
+			position: absolute;
+			top: 0px;
+			right: 0px;
+			display: block;
+			width: 40px;
+		}
+        .event-list > li > .social > ul {
+            border-left: 1px solid rgb(230, 230, 230);
+        }
+		.event-list > li > .social > ul > li {			
+			display: block;
+            padding: 0px;
+		}
+		.event-list > li > .social > ul > li > a {
+			display: block;
+			width: 40px;
+			padding: 10px 0px 9px;
+		}
+	}
 </style>
 <?= $this->endSection() ?>
