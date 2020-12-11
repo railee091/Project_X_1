f018a352b56ad7d1e37d8e9bc54331e8c2746aa2
railee091 <74151748+railee091@users.noreply.github.com>
2020-12-11 19:37:31 +0800

    Update student_files.php

--- app/Views/student/student_files.php
+++ app/Views/student/student_files.php
@@ -4,14 +4,14 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-3">
-			<?php
-			$files=scandir('app/views/files');
-			foreach($files as $file)
-			{
-			    if($file=='.'||$file=='..') continue;
-			    echo '<div><a target="_blank" href="app/views/files/'.$file.'">'. $file.'</a></div>';
-			}
-			?>
+				<?php
+				$files=scandir('app/views/files');
+				foreach($files as $file)
+				{
+				    if($file=='.'||$file=='..') continue;
+				    echo '<a target="_blank" href="app/views/files/'.$file.'"><i class="fas fa-file-pdf fa-10x"></i></a>';
+				}
+				?>
 		</div>
 	</div>
 	
