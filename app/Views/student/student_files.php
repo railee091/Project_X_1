8d962f3ecaf3259c07ad41bc8979bb5b0d765288
railee091 <74151748+railee091@users.noreply.github.com>
2020-12-12 15:16:34 +0800

    library

--- app/Views/student/student_files.php
+++ app/Views/student/student_files.php
@@ -3,17 +3,51 @@
 <?= $this->section('content') ?>
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-3">
+		<div class="col-sm-12">
+            <form class="card card-sm">
+                <div class="card-body row no-gutters align-items-center">
+                    <div class="col-auto">
+                       
+                    </div>
+                    <!--end of col-->
+                    <div class="col">
+                        <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search library">
+                    </div>
+                    <!--end of col-->
+                    <div class="col-auto">
+                        <button class="btn btn-lg btn-success" type="submit"><i class="fas fa-search h4 text-body"></i></button>
+                    </div>
+                    <!--end of col-->
+                </div>
+            </form>
+        </div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="row">
 				<?php
 				$files=scandir('app/views/files');
 				foreach($files as $file)
 				{
 				    if($file=='.'||$file=='..') continue;
-				    echo '<a target="_blank" href="app/views/files/'.$file.'"><i class="fas fa-file-pdf fa-10x"></i></a>';
+				?>
+				<div class="col-sm-2">
+					<a target="_blank" href="app/views/files/<?php echo $file;?>">
+					<div class="bordercard">
+					  	<div class="card-body">
+					  		<i  alt="Card image cap" class="card-img-top fas fa-file-pdf fa-10x"></i>
+					    	<p style="font-size: 10pt;" class="card-title"><?php echo $file; ?></p>
+					  	</div>
+					</div>
+				</a>
+				</div>
+				
+				<?php
 				}
 				?>
+				
+			</div>
 		</div>
 	</div>
-	
 </div>
 <?= $this->endSection() ?>			               
